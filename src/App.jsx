b3b006a74cf378ced33f46de38b17a475b32f588
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2023-09-17 12:54:10 +0530

    404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import HomePage from './Pages/HomePage'
 import AboutUs from './Pages/AboutUs';
+import NotFound from './Pages/NotFound';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path='*' element = {<NotFound />} />
       </Routes>
     </>
   )
